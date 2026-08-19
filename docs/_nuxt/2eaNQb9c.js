d17ec7640738a69f49959de316d3cb3af4ec7c58
Nam Truong <nam.truong@macbook2019.pro>
2026-08-19 21:30:58 +0930

    rename uploaded image files.

--- docs/_nuxt/2eaNQb9c.js
+++
@@ -1,1 +0,0 @@
-import{a2 as s}from"./CfBt4vpg.js";const t=s("/images/about/lagerhalle-aussenansicht.jpg");export{t as _};
---
+++ docs/_nuxt/ICOdTwBW.js
@@ -0,0 +1,1 @@
+import{a2 as s}from"./DGU-W-sL.js";const t=s("/images/about/lagerhalle-aussenansicht.jpg");export{t as _};
