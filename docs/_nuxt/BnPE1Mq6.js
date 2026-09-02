f6f93ca3a14c6ef6017bec662ed22db9e81c6f77
Nam Truong <nam.truong@macbook2019.pro>
2026-09-02 21:39:55 +0930

    adjust order-submission-success message

---
+++ docs/_nuxt/BnPE1Mq6.js
@@ -0,0 +1,1 @@
+import{a5 as s}from"./BvNijP2N.js";const t=s("/images/about/lagerhalle-aussenansicht.jpg");export{t as _};
--- docs/_nuxt/DLRu6gLw.js
+++
@@ -1,1 +0,0 @@
-import{a5 as s}from"./EXU1bR44.js";const t=s("/images/about/lagerhalle-aussenansicht.jpg");export{t as _};
