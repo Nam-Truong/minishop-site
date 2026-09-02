376a8bd47aa5572c181edae4ce36b835fa906ba3
Nam Truong <nam.truong@macbook2019.pro>
2026-09-02 12:25:36 +0930

    add image-zoom-in feature in product page

--- docs/_nuxt/B2DVoVeJ.js
+++
@@ -1,1 +0,0 @@
-import{a3 as s}from"./CL1BZqtp.js";const t=s("/images/about/lagerhalle-aussenansicht.jpg");export{t as _};
---
+++ docs/_nuxt/C_ym66Wz.js
@@ -0,0 +1,1 @@
+import{a5 as s}from"./CeQ3eBgj.js";const t=s("/images/about/lagerhalle-aussenansicht.jpg");export{t as _};
